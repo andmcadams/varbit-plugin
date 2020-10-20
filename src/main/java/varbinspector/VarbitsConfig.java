a991d56ccf0797ef16371505b03db3df27fe808a
Andrew McAdams <andmcadams@gmail.com>
2020-10-20 00:56:52 -0500

    Add in license from Abex's varinspector

--- src/main/java/varbinspector/VarbitsConfig.java
+++ src/main/java/varbinspector/VarbitsConfig.java
@@ -1,5 +1,6 @@
 package varbinspector;
 
+import java.util.UUID;
 import net.runelite.client.config.Config;
 import net.runelite.client.config.ConfigGroup;
 import net.runelite.client.config.ConfigItem;
@@ -7,13 +8,25 @@ import net.runelite.client.config.ConfigItem;
 @ConfigGroup("varbits")
 public interface VarbitsConfig extends Config
 {
+	static final String SESSION_VALUE = UUID.randomUUID().toString();
 	@ConfigItem(
-		keyName = "greeting",
-		name = "Welcome Greeting",
-		description = "The message to show to the user when they login"
+		keyName = "sessionValue",
+		name = "Session ID",
+		description = "Session ID to use when manual toggle is on. Set this first."
 	)
-	default String greeting()
+	default String sessionValue()
 	{
-		return "Hello";
+		return SESSION_VALUE;
 	}
+
+	@ConfigItem(
+		keyName = "sessionToggle",
+		name = "Manual toggle",
+		description = "Use a manually input session ID. Set the session ID before turning on."
+	)
+	default boolean sessionToggle()
+	{
+		return false;
+	}
+
 }
